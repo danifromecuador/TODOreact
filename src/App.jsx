03171e8b63c5c744ae0fa8d00649dbfe02a2008d
dani <danifromecuador@gmail.com>
2023-07-01 11:44:45 -0500

    Fix linter errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 import { Header } from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='main-container'>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import '../styles/Header.css';
 export const Header = () => {
   return (
     <div className='header'>
-      <h1>TODO's React</h1>
+      <h1>TODO&apos;s React</h1>
       <input type="text" id="userInput" placeholder="Enter a new task" />
     </div>
   )
